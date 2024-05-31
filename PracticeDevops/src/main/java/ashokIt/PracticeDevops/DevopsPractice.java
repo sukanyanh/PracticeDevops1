886071cd49c5b59fd1cdfd2826fd7029196ffc8a
sukanya nh <sukanyanh18@gmail.com>
2024-05-31 22:53:26 +0530

    ProgramBySukanya

---
+++ PracticeDevops/src/main/java/ashokIt/PracticeDevops/DevopsPractice.java
@@ -0,0 +1,19 @@
+package ashokIt.PracticeDevops;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DevopsPractice {
+    @GetMapping("/devops")
+    public String print(String s){
+        s="am devops engineer";
+        return s;
+
+    }
+
+
+
+
+}
